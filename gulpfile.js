a4d4d06a4afb80b3e0c481dc12340b192a74b973
Justin O'Neill <mrrioku@yahoo.com>
2016-10-29 08:30:38 -0500

    fixed some bugs, but still have a few

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+    webserver = require('gulp-webserver');
+
+gulp.task('webserver', function() {
+    gulp.src('.')
+        .pipe(webserver({
+            livereload: true,
+            open: true,
+            port: 8080
+        }));
+});
